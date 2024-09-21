787ea9d9e07966de04f5d34da8ae7200ebf27559
tomoya <alligatorfree12@gmail.com>
2024-09-22 07:56:50 +0900

    finish 31. A Basic Event Bus Implementation

---
+++ event-bus/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+// 31. A Basic Event Bus Implementation
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
