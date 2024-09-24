e664bfd73f714f1784001b92c93d77395dc93c8c
tomoya <alligatorfree12@gmail.com>
2024-09-24 09:28:52 +0900

    47. A Quick Test

--- query/index.js
+++ query/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 // 35. Creating the Data Query Service
 // 36. Parsing Incoming Events
 // 43. Adding Comment Moderation
+// 47. A Quick Test
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,20 +24,26 @@ app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
   if (type === "PostCreated") {
-    // post の id, title を取得します。
     const { id, title } = data;
-
-    // posts 配列の の id 番目に、post の id, title, comments: [] を追加します。
     posts[id] = { id, title, comments: [] };
   }
 
   if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
 
-    // posts 配列の post を postId で探します。
+  if (type === "CommentUpdated") {
+    const { id, content, postId, status } = data;
     const post = posts[postId];
 
-    post.comments.push({ id, content, status });
+    const comment = post.comments.find((comment) => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
   }
 
   // event を受け取って、posts 配列に追加した結果を出力します。
